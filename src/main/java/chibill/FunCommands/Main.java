5809cf15f2e0e9ef1d66af13d3d990197a0cdc36
chibill <chibill110@gmail.com>
2015-05-25 09:16:45 -0500

    Fixed me missing an edit by @Phase

--- src/main/java/chibill/FunCommands/Main.java
+++ src/main/java/chibill/FunCommands/Main.java
@@ -92,7 +92,7 @@ public class Main extends JavaPlugin implements Listener {
     			
     			int temp =(int)(Math.random()*Foods.length);
     			
-    			Bukkit.broadcastMessage("§5" + sender.getName() +"§e threw a" + Foods[temp].name().matches("^[aeiou].*") ? "n" : "" + " §6 " + Foods[temp].name() +"§c at §5" + victim.getName());
+    			Bukkit.broadcastMessage("§5" + sender.getName() +"§e threw a" + (Foods[temp].name().matches("^[aeiou].*") ? "n" : "") + " §6 " + Foods[temp].name() +"§c at §5" + victim.getName());
     		if(victim.getGameMode() != GameMode.SURVIVAL & victim.getGameMode() !=  GameMode.ADVENTURE ){
     			victim.getInventory().addItem(new ItemStack(Foods[temp]));
     		}
@@ -194,7 +194,7 @@ public class Main extends JavaPlugin implements Listener {
     			}catch(Exception e){
     			}
                 
-                Bukkit.broadcastMessage("§5" + sender.getName() + "§c slapped §5" + victimName + "§c about a bit with a/an §6" +slap);
+                Bukkit.broadcastMessage("§5" + sender.getName() + "§c slapped §5" + victimName + "§c about a bit with a" + (slap.matches("^[aeiou].*") ? "n" : "") +  "§6" +slap);
                 
     		}else{
     			sender.sendMessage("§c[ERROR] You must specify who you are slapping");
